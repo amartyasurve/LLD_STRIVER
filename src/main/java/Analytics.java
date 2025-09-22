a21609ddbcb7e691b9d6fc2f5ff9b9f14abf5118
amartyasurve <amartyasurve@gmail.com>
2025-09-22 23:10:26 +0530

    SingleTon pattern - eager loading

--- src/main/java/Analytics.java
+++ src/main/java/Analytics.java
@@ -1,22 +1,30 @@
 public class Analytics {
-    private int run = 0;
-    private int submit = 0;
+    //    private int run = 0;
+//    private int submit = 0;
+//
+//    public Analytics() {}
+//
+//    public void countRun() {
+//        run++;
+//    }
+//
+//    public void countSubmit() {
+//        submit++;
+//    }
+//
+//    public int getSubmit() {
+//        return submit;
+//    }
+//
+//    public int getRun() {
+//        return run;
+//    }
+    private static final Analytics analytics = new Analytics();
 
-    public Analytics() {}
+    private Analytics(){}
 
-    public void countRun() {
-        run++;
+    public static Analytics getAnalytics(){
+        return analytics;
     }
 
-    public void countSubmit() {
-        submit++;
-    }
-
-    public int getSubmit() {
-        return submit;
-    }
-
-    public int getRun() {
-        return run;
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Analytics analytics1 = new Analytics();//two different object of the same class
-        analytics1.countRun();
-
-        Analytics analytics2 = new Analytics();
-        analytics2.countRun();
-
+        Analytics analytics1=Analytics.getAnalytics();
+        Analytics analytics2=Analytics.getAnalytics();
         System.out.println(analytics1);
         System.out.println(analytics2);
     }
